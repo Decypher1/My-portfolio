7c4fd5d731634f86807b7eee8bdfcd119d391cfd
decypher1 <mumekwe2@gmail.com>
2022-06-17 17:45:35 +0100

    updated server file

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 const http = require('http');
 
-// using the  file  system to fetch the html file
-const fs = require('fs');
-// creating a server
+const router = require('./routes');
 
 let handleRequest = (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.write(data);
-        }
-        // end() method to show we are done handling the request
-        response.end();
-    });
 }
 
-http.createServer(handleRequest).listen(4000);
+http.createServer(router.handleRequest).listen(4000);
